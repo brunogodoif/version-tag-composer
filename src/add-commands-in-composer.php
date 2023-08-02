bb00c5cb752a64f9e3d5694b24d9a280bc59dfd1
Bruno Godoi <brunofgodoi@outloo.com.br>
2023-08-02 11:30:28 -0300

    add comandos projeto principal

---
+++ src/add-commands-in-composer.php
@@ -0,0 +1,38 @@
+<?php
+
+echo dirname(__DIR__, 4) . "\n";
+
+// Caminho para o arquivo composer.json do projeto principal
+$composerJsonPath = dirname(__DIR__, 4) . '/composer.json';
+// Lê o conteúdo atual do composer.json
+$content = file_get_contents($composerJsonPath);
+
+// Decodifica o conteúdo em um array associativo
+$data = json_decode($content, true);
+
+$linesToAdd = [
+    'app-version' => 'php version-tag-get.php',
+    'app-version-patch' => 'php version-tag-increment.php --mode=patch',
+    'app-version-minor' => 'php version-tag-increment.php --mode=minor',
+    'app-version-major' => 'php version-tag-increment.php --mode=major',
+];
+
+// Verifica se a tag "scripts" já existe
+if (!isset($data['scripts'])) {
+    // Se não existir, cria a tag "scripts" e adiciona os novos scripts
+    $data['scripts'] = $linesToAdd;
+} else {
+    // Se já existir, adiciona apenas os novos scripts
+    foreach ($linesToAdd as $key => $lineAdd) {
+        if (!isset($data['scripts'][$key])) {
+            $data['scripts'][$key] = $lineAdd;
+        }
+    }
+}
+
+// Codifica o array de volta para o formato JSON
+$updatedComposerJson = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+// Escreve o conteúdo atualizado de volta no arquivo composer.json
+file_put_contents($composerJsonPath, $updatedComposerJson);
+?>
